60af6c07f7f4de95f82e0f89800b8768777034de
Hamit Can Uzunay <canuzunay@gmail.com>
2022-08-24 19:19:22 +0300

    Code refactor

--- account-service/src/main/java/com/hcu/accountservice/controller/impl/AccountController.java
+++ account-service/src/main/java/com/hcu/accountservice/controller/impl/AccountController.java
@@ -11,11 +11,6 @@ import com.hcu.accountservice.vo.response.IResponseVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,31 +22,26 @@ public class AccountController extends BaseController implements IAccountControl
     private final AccountService accountService;
 
     @Override
-    @GetMapping(ApiPath.AccountCtrl.GET)
-    public ResponseEntity<IResponseVO> get(@PathVariable("id") String id) {
+    public ResponseEntity<IResponseVO> get(String id) {
         return ok(ResponseMessage.SUCCESS, accountService.get(id));
     }
 
     @Override
-    @PostMapping
     public ResponseEntity<IResponseVO> save(CreateAccountVO createAccountVO) {
         return ok(ResponseMessage.SUCCESS, accountService.save(createAccountVO));
     }
 
     @Override
-    @PutMapping
     public ResponseEntity<IResponseVO> update(UpdateAccountVO updateAccountVO) {
         return ok(ResponseMessage.SUCCESS, accountService.update(updateAccountVO));
     }
 
     @Override
-    @DeleteMapping(ApiPath.AccountCtrl.DELETE)
-    public void delete(@PathVariable("id") String id) {
+    public void delete(String id) {
         accountService.delete(id);
     }
 
     @Override
-    @GetMapping
     public ResponseEntity<IResponseVO> getAll(Pageable pageable) {
         return ok(ResponseMessage.SUCCESS, accountService.getAll(pageable));
     }
